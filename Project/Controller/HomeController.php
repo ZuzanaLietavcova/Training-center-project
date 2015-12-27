15505096d44e34a69b8ee40a9d77c5a7777d8d00
Niels Dawartz <nillernoels@NillerNoels.local>
2015-12-27 18:44:41 +0100

    updated views

--- Project/Controller/HomeController.php
+++ Project/Controller/HomeController.php
@@ -17,8 +17,34 @@ $studentID = $_SESSION['student_id'];
 
 if ($studentID != "")
 {
-    echo "student";
-    include_once "../view/home-student.php";
+    $studentName = $_SESSION['name'];       // get name
+    $projectPerPage = 4;
+    $content = "";
+    $totalNumberOfProjects = count(ProjectModel::getAllProjects(1));        // get total amount
+    $totalNumberOfPages = ceil($totalNumberOfProjects / $projectPerPage);  // calc number of pages
+    $currentPage = (isset($_GET['current_page'])) ? (int)$_GET['current_page'] : 1;     // if currentPage var isset else default 1
+    $startPage = ($currentPage - 1) * $projectPerPage;      // calc start page ex. 1 = 0 - 4 projects
+
+    // set content:
+    $listOfProjects = ProjectModel::getCurrentProjects($studentID, $startPage, $projectPerPage);  // get projects
+    $content = "";
+
+    for ($int = 0; $int < count($listOfProjects); $int++)                   // max 4 projects on page
+    {
+        $name = $listOfProjects[$int]['Title'];
+        $project_id = $listOfProjects[$int]['project_id'];
+        $content .= "<div class=\"col-md-3 portfolio-item\">
+                           <a href=\"some-link/$project_id\">
+                                <img class=\"img-responsive\" src=\"http://placehold.it/750x450\" alt=\"\">
+                           </a>
+                           <p style='text-align: center'> $name </p>
+                     </div>";
+    }
+
+    $pagination = WebFunctions::pagination($currentPage, $totalNumberOfPages);   // add pagination
+
+    // add view
+    include_once "../view/home-trainer.php";
 }
 else if ($_SESSION['trainer_id'] != "")
 {
