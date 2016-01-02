928fbc61db5cd9701c039ad20c7152f64f20c686
Zuzka <Zuzka@bitbucket.com>
2016-01-02 19:35:45 +0100

    ProjectModel: get "free" students query

--- Project/model/ProjectModel.php
+++ Project/model/ProjectModel.php
@@ -95,7 +95,7 @@ class ProjectModel
     public static function getProjectById($project_id)
     {
         $db = Db::getConnection();
-        $sql = "SELECT project_id, title, subject, creation_time, deadline,
+        $sql = "SELECT p.project_id, p.title, p.subject, p.creation_time, p.deadline, p.class_id,
                     t.name AS trainer, t.trainer_id AS trainer_id, c.name AS class
                     FROM Project p
                     INNER JOIN Trainer t ON t.Trainer_id=p.project_id
@@ -166,4 +166,30 @@ class ProjectModel
         }
     }
 
+    public static function getFreeStudents($class_id)
+    {
+        // Gets students not in any team
+        $db = Db::getConnection();
+        $sql = "SELECT s.student_id 
+            FROM   study s 
+            LEFT   JOIN member m  ON s.student_id=m.student_id
+            WHERE  m.student_id IS NULL";
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":class_id", $class_id);
+
+        $ok = $stmt->execute();
+        if($ok)
+        {
+            return  $stmt->fetchall(PDO::FETCH_ASSOC);
+        }
+        else
+        {
+            $error = $stmt->errorInfo();    // else print error codes
+            echo $error[0];
+            echo $error[1];
+            echo $error[2];
+            return 0;
+        }
+    }
 }
